2b0c1f5abab9d0fd7407eceb967e896d4ea87db2
DrissiOmar <omar.drissi@esprit.tn>
2025-11-08 16:03:54 +0100

    refactor(product-card): remove internal wishlist logic and enable content projection for reusable action buttons

--- src/app/shared/components/product-card/product-card.ts
+++ src/app/shared/components/product-card/product-card.ts
@@ -1,33 +1,23 @@
-import {Component, inject, input, output} from '@angular/core';
+import {Component, input} from '@angular/core';
 import {Product} from '../../models/product';
 import {MatIcon} from '@angular/material/icon';
-import {MatButton, MatIconButton} from '@angular/material/button';
-import {FavoritesStore} from '../../../core/store/favorites.store';
+import {MatButton} from '@angular/material/button';
+
 
 @Component({
   selector: 'app-product-card',
   imports: [
     MatIcon,
     MatButton,
-    MatIconButton
+
   ],
   templateUrl: './product-card.html',
   styleUrl: './product-card.scss',
 })
 export class ProductCard {
 
-  favoriteStore = inject(FavoritesStore);
-
   product = input.required<Product>();
 
 
-  toggleWishList(product: Product) {
-    if (this.favoriteStore.isFavorite()(product.id)) {
-      this.favoriteStore.removeFromFavorites(product.id);
-    } else {
-      this.favoriteStore.addToFavorites(product);
-    }
 
-
-  }
 }
