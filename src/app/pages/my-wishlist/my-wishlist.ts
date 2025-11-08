91f26ef9c6fc670f108f6f935d3302cb2f88bf06
DrissiOmar <omar.drissi@esprit.tn>
2025-11-08 15:12:18 +0100

    feat(wishlist): implement MyWishlist page with reusable BackButton, ProductCard integration, and FavoritesStore state management

--- src/app/pages/my-wishlist/my-wishlist.ts
+++ src/app/pages/my-wishlist/my-wishlist.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {BackButton} from '../../shared/components/back-button/back-button';
+import {FavoritesStore} from '../../core/store/favorites.store';
+import {ProductCard} from '../../shared/components/product-card/product-card';
 
 @Component({
   selector: 'app-my-wishlist',
-  imports: [],
+  imports: [
+    BackButton,
+    ProductCard
+  ],
   templateUrl: './my-wishlist.html',
   styleUrl: './my-wishlist.scss',
 })
 export class MyWishlist {
+
+  favoriteStore = inject(FavoritesStore);
 
 }
