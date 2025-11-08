13a6d72bc927653366895d47a6703a903daa84cd
DrissiOmar <omar.drissi@esprit.tn>
2025-11-08 14:26:47 +0100

    feat(header): integrate FavoritesStore badge in header actions with real-time count using Angular Material MatBadge

--- src/app/layout/header-actions/header-actions.ts
+++ src/app/layout/header-actions/header-actions.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
 import {MatButton, MatIconButton} from '@angular/material/button';
 import {MatIcon} from '@angular/material/icon';
 import {RouterLink} from '@angular/router';
+import {FavoritesStore} from '../../core/store/favorites.store';
+import {MatBadge} from '@angular/material/badge';
 
 
 @Component({
@@ -10,11 +12,14 @@ import {RouterLink} from '@angular/router';
     MatButton,
     MatIconButton,
     MatIcon,
-    RouterLink
+    RouterLink,
+    MatBadge
   ],
   templateUrl: './header-actions.html',
   styleUrl: './header-actions.scss',
 })
 export class HeaderActions {
+
+  favoriteStore = inject(FavoritesStore);
 
 }
