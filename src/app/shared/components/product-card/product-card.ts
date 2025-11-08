9b5fdfe6b4be16b2cb24e6232d8ec588a98002b9
DrissiOmar <omar.drissi@esprit.tn>
2025-11-08 14:25:17 +0100

    feat(product-card): integrate FavoritesStore for wishlist toggle with dynamic icon state and improved UI feedback

--- src/app/shared/components/product-card/product-card.ts
+++ src/app/shared/components/product-card/product-card.ts
@@ -1,21 +1,33 @@
-import {Component, input, output} from '@angular/core';
+import {Component, inject, input, output} from '@angular/core';
 import {Product} from '../../models/product';
 import {MatIcon} from '@angular/material/icon';
-import {MatButton} from '@angular/material/button';
+import {MatButton, MatIconButton} from '@angular/material/button';
+import {FavoritesStore} from '../../../core/store/favorites.store';
 
 @Component({
   selector: 'app-product-card',
   imports: [
     MatIcon,
-    MatButton
+    MatButton,
+    MatIconButton
   ],
   templateUrl: './product-card.html',
   styleUrl: './product-card.scss',
 })
 export class ProductCard {
 
+  favoriteStore = inject(FavoritesStore);
+
   product = input.required<Product>();
 
 
+  toggleWishList(product: Product) {
+    if (this.favoriteStore.isFavorite()(product.id)) {
+      this.favoriteStore.removeFromFavorites(product.id);
+    } else {
+      this.favoriteStore.addToFavorites(product);
+    }
 
+
+  }
 }
