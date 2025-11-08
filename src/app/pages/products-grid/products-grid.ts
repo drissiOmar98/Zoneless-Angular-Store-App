b2dec8f2258430ce6442118996ccfafef68094fb
DrissiOmar <omar.drissi@esprit.tn>
2025-11-08 16:04:51 +0100

    refactor(products-grid): integrate ToggleWishlistButton for reusable wishlist actions and clean component logic

--- src/app/pages/products-grid/products-grid.ts
+++ src/app/pages/products-grid/products-grid.ts
@@ -1,11 +1,14 @@
-import {Component, computed, inject, input, signal} from '@angular/core';
-import {Product} from '../../shared/models/product';
+import {Component, inject, input, signal} from '@angular/core';
+
 import {ProductCard} from '../../shared/components/product-card/product-card';
 import {MatSidenav, MatSidenavContainer, MatSidenavContent} from '@angular/material/sidenav';
 import {MatListItem, MatListItemTitle, MatNavList} from '@angular/material/list';
 import {RouterLink} from '@angular/router';
 import {TitleCasePipe} from '@angular/common';
 import {EcommerceStore} from '../../core/store/ecommerce-store';
+import {MatIcon} from '@angular/material/icon';
+import {MatIconButton} from '@angular/material/button';
+import {ToggleWishlistButton} from '../../shared/components/toggle-wishlist-button/toggle-wishlist-button';
 
 @Component({
   selector: 'app-products-grid',
@@ -19,6 +22,7 @@ import {EcommerceStore} from '../../core/store/ecommerce-store';
     MatListItemTitle,
     RouterLink,
     TitleCasePipe,
+    ToggleWishlistButton,
   ],
   templateUrl: './products-grid.html',
   styleUrl: './products-grid.scss',
@@ -45,10 +49,6 @@ export class ProductsGrid {
   constructor() {
     this.store.setCategory(this.category);
   }
-
-
-
-
 
 
 }
