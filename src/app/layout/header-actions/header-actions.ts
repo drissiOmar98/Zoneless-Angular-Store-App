eae4a9d6e6a4811db73d334d5a8ea8ce692d619f
DrissiOmar <omar.drissi@esprit.tn>
2025-11-18 23:55:48 +0100

    feat(header): integrate authentication dialogs, enhance user menu with MatMenu components, and implement dynamic UI logic based on user session state

--- src/app/layout/header-actions/header-actions.ts
+++ src/app/layout/header-actions/header-actions.ts
@@ -8,6 +8,9 @@ import {CartStore} from '../../core/store/cart.store';
 import {EcommerceStore} from '../../core/store/ecommerce-store';
 import {MatMenu, MatMenuItem, MatMenuTrigger} from '@angular/material/menu';
 import {MatDivider} from '@angular/material/divider';
+import {MatDialog} from '@angular/material/dialog';
+import {SignUpDialog} from '../../shared/components/sign-up-dialog/sign-up-dialog';
+import {SignInDialog} from '../../shared/components/sign-in-dialog/sign-in-dialog';
 
 
 @Component({
@@ -31,5 +34,17 @@ export class HeaderActions {
   favoriteStore = inject(FavoritesStore);
   cartStore = inject(CartStore);
   store = inject(EcommerceStore);
+  matDialog = inject(MatDialog);
 
+  openSignInDialog() {
+    this.matDialog.open(SignInDialog, {
+      disableClose: true,
+    });
+  }
+
+  openSignUpDialog() {
+    this.matDialog.open(SignUpDialog, {
+      disableClose: true,
+    });
+  }
 }
