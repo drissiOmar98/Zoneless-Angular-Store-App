7fb3528f537d1336ce9488be17694fc3eef3ef9a
DrissiOmar <omar.drissi@esprit.tn>
2025-11-19 00:07:54 +0100

    feat(checkout): add fully responsive ShippingForm component with structured address inputs and semantic layout for streamlined checkout experience

---
+++ src/app/pages/checkout/shipping-form/shipping-form.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import {ViewPanel} from '../../../shared/directives/view-panel';
+import {MatIcon} from '@angular/material/icon';
+import {MatFormField} from '@angular/material/form-field';
+import {MatInput} from '@angular/material/input';
+
+@Component({
+  selector: 'app-shipping-form',
+  imports: [
+    ViewPanel,
+    MatIcon,
+    MatFormField,
+    MatInput
+  ],
+  templateUrl: './shipping-form.html',
+  styleUrl: './shipping-form.scss',
+})
+export class ShippingForm {
+
+}
