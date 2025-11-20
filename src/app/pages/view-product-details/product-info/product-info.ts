d9c36c741dad33f8ee0e0406f2588f3f63686217
DrissiOmar <omar.drissi@esprit.tn>
2025-11-20 21:05:34 +0100

    feat(product-info): add product details view with stock status, quantity selector, add-to-cart, wishlist toggle, and shipping & warranty info

---
+++ src/app/pages/view-product-details/product-info/product-info.ts
@@ -0,0 +1,30 @@
+import {Component, inject, input, Input, signal} from '@angular/core';
+import {Product} from '../../../shared/models/product';
+import {MatIcon} from '@angular/material/icon';
+import {ToggleWishlistButton} from '../../../shared/components/toggle-wishlist-button/toggle-wishlist-button';
+import {MatButton, MatIconButton} from '@angular/material/button';
+import {QtySelector} from '../../../shared/components/qty-selector/qty-selector';
+import {TitleCasePipe} from '@angular/common';
+import {CartStore} from '../../../core/store/cart.store';
+import {StockStatus} from '../stock-status/stock-status';
+
+@Component({
+  selector: 'app-product-info',
+  imports: [
+    MatIcon,
+    ToggleWishlistButton,
+    MatIconButton,
+    QtySelector,
+    MatButton,
+    TitleCasePipe,
+    StockStatus
+  ],
+  templateUrl: './product-info.html',
+  styleUrl: './product-info.scss',
+})
+export class ProductInfo {
+  product = input.required<Product>();
+  cartStore = inject(CartStore);
+  quantity = signal(1);
+
+}
--- src/app/pages/view-product-details/view-product-details.ts
+++ src/app/pages/view-product-details/view-product-details.ts
@@ -1,11 +1,13 @@
 import {Component, computed, inject, input} from '@angular/core';
 import {EcommerceStore} from '../../core/store/ecommerce-store';
 import {BackButton} from '../../shared/components/back-button/back-button';
+import {ProductInfo} from './product-info/product-info';
 
 @Component({
   selector: 'app-view-product-details',
   imports: [
-    BackButton
+    BackButton,
+    ProductInfo
   ],
   templateUrl: './view-product-details.html',
   styleUrl: './view-product-details.scss',
