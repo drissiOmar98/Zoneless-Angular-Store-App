6ad58e363cd7c9ae5ef378ab7085ac34867aad92
DrissiOmar <omar.drissi@esprit.tn>
2025-12-01 22:53:43 +0100

    feat(reviews): enhance ViewReviews component with conditional write-review form and user-triggered review submission

--- src/app/pages/view-product-details/view-reviews/view-reviews.ts
+++ src/app/pages/view-product-details/view-reviews/view-reviews.ts
@@ -1,20 +1,27 @@
-import {Component, computed, input} from '@angular/core';
+import {Component, computed, inject, input} from '@angular/core';
 import {Product} from '../../../shared/models/product';
 import {ViewPanel} from '../../../shared/directives/view-panel';
 import {RatingSummary} from '../rating-summary/rating-summary';
 import {ViewReviewItem} from '../view-review-item/view-review-item';
+import {MatButton} from '@angular/material/button';
+import {EcommerceStore} from '../../../core/store/ecommerce-store';
+import {WriteReview} from '../write-review/write-review';
 
 @Component({
   selector: 'app-view-reviews',
   imports: [
     ViewPanel,
     RatingSummary,
-    ViewReviewItem
+    ViewReviewItem,
+    MatButton,
+    WriteReview
   ],
   templateUrl: './view-reviews.html',
   styleUrl: './view-reviews.scss',
 })
 export class ViewReviews {
+
+  store = inject(EcommerceStore);
 
   product = input.required<Product>();
 
