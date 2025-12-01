a3256e076654ccc0f20402bd347faab34cf97cd4
DrissiOmar <omar.drissi@esprit.tn>
2025-12-01 22:50:48 +0100

    feat(store): add addReview handler to create user reviews, update product ratings/count, and manage writeReview state

--- src/app/core/store/ecommerce-store.ts
+++ src/app/core/store/ecommerce-store.ts
@@ -1481,7 +1481,40 @@ export const EcommerceStore = signalStore(
     hideWriteReview: () => {
       patchState(store, {writeReview: false});
     },
+    addReview: async ( {title, comment, rating}: AddReviewParams) => {
+      patchState(store, {loading: true});
+      const product = store.products().find((p) => p.id === store.selectedProductId());
+      if(!product){
+        toaster.error('Product not found');
+        patchState(store, {loading: false});
+        return;
+      }
 
+      const review: UserReview = {
+        id: crypto.randomUUID(),
+        title,
+        comment,
+        rating,
+        productId: product.id,
+        userName: store.user()?.name || '',
+        userImageUrl: store.user()?.imageUrl || '',
+        reviewDate: new Date(),
+      };
+
+      const updatedProducts = produce(store.products(), (draft) => {
+        const index = draft.findIndex((p) => p.id === product.id);
+        draft[index].reviews.push(review);
+        draft[index].rating =
+          Math.round(
+            (draft[index].reviews.reduce((acc,r) => acc + r.rating, 0) /
+                draft[index].reviews.length) * 10,
+          ) / 10;
+        draft[index].reviewCount= draft[index].reviews.length;
+      });
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+
+      patchState(store, {loading: false, products: updatedProducts, writeReview: false});
+    },
 
 
 
