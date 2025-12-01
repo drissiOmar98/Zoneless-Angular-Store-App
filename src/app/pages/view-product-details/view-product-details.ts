8adaa08465cad7e1a7d838510c7efea154601014
DrissiOmar <omar.drissi@esprit.tn>
2025-12-01 21:00:45 +0100

    feat(product-details): add fully integrated customer reviews section

--- src/app/pages/view-product-details/view-product-details.ts
+++ src/app/pages/view-product-details/view-product-details.ts
@@ -2,12 +2,15 @@ import {Component, computed, inject, input} from '@angular/core';
 import {EcommerceStore} from '../../core/store/ecommerce-store';
 import {BackButton} from '../../shared/components/back-button/back-button';
 import {ProductInfo} from './product-info/product-info';
+import {ViewReviews} from './view-reviews/view-reviews';
+import {ViewReviewItem} from './view-review-item/view-review-item';
 
 @Component({
   selector: 'app-view-product-details',
   imports: [
     BackButton,
-    ProductInfo
+    ProductInfo,
+    ViewReviews,
   ],
   templateUrl: './view-product-details.html',
   styleUrl: './view-product-details.scss',
@@ -21,6 +24,6 @@ export class ViewProductDetails {
     this.store.setProductId(this.productId);
   }
 
-   backRoute = computed(() => `/products/${this.store.category()}`);
+  backRoute = computed(() => `/products/${this.store.category()}`);
 
 }
